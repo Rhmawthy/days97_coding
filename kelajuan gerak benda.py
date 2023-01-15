bef71bf0aa11eb15cfa81995ebf1b437cbfbf51e
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2023-01-15 20:23:02 +0800

    Add files via upload

---
+++ kelajuan gerak benda.py
@@ -0,0 +1,8 @@
+#program menentukan kelajuan gerak benda
+
+jarak = int(input("masukkan jarak yang di tempuh dalam satuan meter: "))
+waktu = int(input("masukkan Selan waktu dalam satuan detik:"))
+
+kelajuan = jarak / waktu
+
+print (" kelajuan gerak benda =",kelajuan)
